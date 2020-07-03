8fbebe862de3d8c5500559c9d72ce624b835bcde
Pedro Giuliano Farina <pedrogfarina@gmail.com>
2020-07-03 14:26:24 -0300

    Initial Commit

---
+++ Beatbots/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Beatbots
+//
+//  Created by Pedro Giuliano Farina on 03/07/20.
+//  Copyright © 2020 Pedro Giuliano Farina. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
